c9c41cb266619b7e69adc49fabdc1ecfc1636b00
Marcus Gabriel <marcusgabriel.ds@gmail.com>
2022-02-08 08:49:30 -0300

    Add echo-extended

---
+++ echo-extended/server/echo-extended.py
@@ -0,0 +1,58 @@
+# Copyright 2022 Cartesi Pte. Ltd.
+#
+# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+
+from os import environ
+import logging
+import requests
+import json
+from flask import Flask, request
+
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+
+dispatcher_url = environ["HTTP_DISPATCHER_URL"]
+app.logger.info(f"HTTP dispatcher url is {dispatcher_url}")
+
+
+@app.route("/advance", methods=["POST"])
+def advance():
+    body = request.get_json()
+    app.logger.info(f"Received advance request body {body}")
+    app.logger.info("Printing Body Payload : "+ body["payload"])
+
+    partial = body["payload"][2:]
+    app.logger.info("Hex Without 0x : "+ partial)
+
+    content = (bytes.fromhex(hex).decode("utf-8"))
+
+    app.logger.info("Message before conversion: " + content)    
+    content_upper = content.upper()
+    app.logger.info("Message after conversion: " + content_upper)
+
+    newpayload = "0x" + content_upper.encode("utf-8").hex()
+    app.logger.info("Operation Result in Hex: " + newpayload)
+    body["payload"] = newpayload
+    app.logger.info("New PayLoad Added: "+body["payload"])
+    app.logger.info("Adding notice")
+    response = requests.post(dispatcher_url + "/notice", json={"payload": body["payload"]})
+    app.logger.info(f"Received notice status {response.status_code} body {response.content}")
+    app.logger.info("Finishing")
+    response = requests.post(dispatcher_url + "/finish", json={"status": "accept"})
+    app.logger.info(f"Received finish status {response.status_code}")
+    return "", 202
+
+
+@app.route("/inspect/<payload>", methods=["GET"])
+def inspect(payload):
+    app.logger.info(f"Received inspect request payload {payload}")
+    return {"reports": [{"payload": payload}]}, 200
