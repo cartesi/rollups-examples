3266be28d083b05cefd9d8fa7a39f6fbc45e028c
Marcel Moura <marcel.moura@cartesi.io>
2022-03-08 22:54:45 -0300

    Add documentation and some sample back-end code

--- custom-dapps/template/server/template.py
+++ custom-dapps/template/server/template.py
@@ -21,9 +21,63 @@ app.logger.setLevel(logging.INFO)
 dispatcher_url = environ["HTTP_DISPATCHER_URL"]
 app.logger.info(f"HTTP dispatcher url is {dispatcher_url}")
 
+def hex2str(hex):
+    """
+    Decodes a hex string into a regular string
+    """
+    return bytes.fromhex(hex[2:]).decode("utf-8")
+
+def str2hex(str):
+    """
+    Encodes a string as a hex string
+    """
+    return "0x" + str.encode("utf-8").hex()
 
 @app.route("/advance", methods=["POST"])
 def advance():
+    """
+    An advance request may be processed as follows:
+
+    1. The body of the advance request may be retrieved to be used later on:
+
+    body = request.get_json()
+    app.logger.info(f"Received advance request body {body}")
+
+    2. A notice may be generated, if appropriate:
+
+    response = requests.post(dispatcher_url + "/notice", json={"payload": payload})
+    app.logger.info(f"Received notice status {response.status_code} body {response.content}")
+
+    3. During processing, any expection must be handled accordingly:
+
+    try:
+        # Execute sensible operation
+        op.execute(params)
+
+    except Exception as e:
+        # status must be "reject"
+        status = "reject"
+        msg = "Error executing operation"
+        app.logger.error(msg)
+        response = requests.post(dispatcher_url + "/report", json={"payload": str2hex(msg)})
+
+    finally:
+        # Close any resource, if necessary
+        res.close()
+
+    4. Finish processing
+
+    app.logger.info("Finishing")
+    response = requests.post(dispatcher_url + "/finish", json={"status": status})
+    app.logger.info(f"Received finish status {response.status_code}")
+    return "", 202
+    """
+
+    """
+    The sample code fomr the Echo DApp simply generates a notice with the payload of the
+    request and print some log messages.
+    """
+
     body = request.get_json()
     app.logger.info(f"Received advance request body {body}")
     app.logger.info("Adding notice")
@@ -33,7 +87,6 @@ def advance():
     response = requests.post(dispatcher_url + "/finish", json={"status": "accept"})
     app.logger.info(f"Received finish status {response.status_code}")
     return "", 202
-
 
 @app.route("/inspect/<payload>", methods=["GET"])
 def inspect(payload):
