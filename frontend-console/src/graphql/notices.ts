cb60c819e97c8a033ab1d136700512f58cb891c0
Milton Jonathan <milton.jonathan@cartesi.io>
2022-06-28 12:26:56 -0300

    Add frontend-console command to validate notice
    Co-authored-by: Marcel Moura <marcel.moura@cartesi.io>

--- frontend-console/src/graphql/notices.ts
+++ frontend-console/src/graphql/notices.ts
@@ -17,6 +17,7 @@ import {
     NoticesByEpochAndInputDocument,
     Notice,
     Input,
+    NoticeDocument,
 } from "../../generated-src/graphql";
 import { InputKeys } from "../commands/types";
 
@@ -35,7 +36,7 @@ const isPartialNotice = (n: PartialNotice | null): n is PartialNotice =>
  * Queries a GraphQL server for notices based on input keys
  * @param url URL of the GraphQL server
  * @param input input identification keys
- * @returns List of notices
+ * @returns List of notices, returned as PartialNotice objects
  */
 export const getNotices = async (
     url: string,
@@ -99,5 +100,29 @@ export const getNotices = async (
         } else {
             return [];
         }
+    }
+};
+
+/**
+ * Queries a GraphQL server looking for a specific notice
+ * @param url URL of the GraphQL server
+ * @param id ID of the notice
+ * @returns The corresponding notice, returned as a full Notice object
+ */
+export const getNotice = async (url: string, id: string): Promise<Notice> => {
+    // create GraphQL client to reader server
+    const client = createClient({ url, exchanges: defaultExchanges, fetch });
+
+    // query the GraphQL server for the notice
+    console.log(`querying ${url} for notice "${id}"...`);
+
+    const { data, error } = await client
+        .query(NoticeDocument, { id })
+        .toPromise();
+
+    if (data?.notice) {
+        return data.notice as Notice;
+    } else {
+        throw new Error(error?.message);
     }
 };
