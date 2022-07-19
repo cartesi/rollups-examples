47f8afc8c8f1ffc2edec41894d7a47489d769db4
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-07-19 16:44:02 -0300

    packages installation

---
+++ frontend-m2cgen/src/App.tsx
@@ -0,0 +1,15 @@
+
+import styled from "styled-components"
+
+export const App = () => {
+  return (
+    <AppWrapper>
+        <Heading>Hello world</Heading>
+    </AppWrapper>
+  )
+}
+
+const AppWrapper = styled.div``
+const Heading = styled.h1`
+    color: red;
+`
---
+++ frontend-m2cgen/src/utils/app.constants.ts
@@ -0,0 +1,7 @@
+export const brandName = "Titanic Survivor"
+
+export const metadata = {
+    app: {
+        title: brandName,
+    },
+};
