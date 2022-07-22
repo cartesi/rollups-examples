0d9591f47ad16ea55b03e2aa3e8ad653569d743a
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-07-22 13:52:16 -0300

    notices and send services adjusted

--- frontend-m2cgen/src/service/notices.ts
+++ frontend-m2cgen/src/service/notices.ts
@@ -9,11 +9,9 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-import { createClient, defaultExchanges } from "@urql/core";
-import fetch from "cross-fetch";
 import { ethers } from "ethers";
 import { GetNoticeDocument, Notice, NoticeKeys } from "../../generated/graphql";
-import { DEFAULT_URL } from "./configs/constants";
+import { client } from "./configs/client";
 
 // we don't get every field of Notice
 export type PartialNotice = Pick<
@@ -31,35 +29,30 @@ export interface NoticeViewModel extends PartialNotice {
 }
 
 // define a type predicate to filter out notices
-const isPartialNotice = (n: PartialNotice | null): n is PartialNotice =>
+export const isPartialNotice = (n: PartialNotice | null): n is PartialNotice =>
     n !== null;
 
 /**
  * Queries a GraphQL server looking for the notices of an input
- * @param url URL of the GraphQL server
  * @param input Blockchain event of input added or the notice keys to be queried
  * @param timeout How long to wait for notice to be detected
  * @returns List of notices
  */
+
 export const getNotices = async (
     noticeKeys: NoticeKeys,
-    url: string = DEFAULT_URL
+    noCache?: boolean
 ): Promise<NoticeViewModel[]> => {
-    // create GraphQL client to reader server
-    const client = createClient({
-        url,
-        exchanges: defaultExchanges,
-        fetch
-    });
-
     // query the GraphQL server for notices of our input
-    // keeping trying forever (or until user kill the process)
-    console.log(
-        `querying ${url} for notices of ${JSON.stringify(noticeKeys)}...`
-    );
     const { data, error } = await client
-        .query(GetNoticeDocument, { query: noticeKeys })
-        .toPromise();
+        .query({
+            query: GetNoticeDocument,
+            variables: {
+                query: noticeKeys
+            },
+            fetchPolicy: noCache ? 'network-only' : 'cache-first'
+        })
+
     if (data?.GetNotice) {
         return data.GetNotice.filter(isPartialNotice).map(
             (partialNotice: PartialNotice) => ({
@@ -69,7 +62,9 @@ export const getNotices = async (
                 ),
             })
         );
+    } else if (error?.message) {
+        throw new Error(error.message);
     } else {
-        throw new Error(error?.message);
+        return [];
     }
 };
--- frontend-m2cgen/src/service/send.ts
+++ frontend-m2cgen/src/service/send.ts
@@ -1,11 +1,23 @@
-import { Args as ConnectArgs, connect } from "./configs/connect";
-import { Args as RollupsArgs, rollups } from "./configs/rollups";
+import { connect } from "./configs/connect";
+import { rollups } from "./configs/rollups";
 import { ContractReceipt, ethers } from "ethers";
-import { NoticeKeys } from "../../generated/graphql";
+import { InputMaybe, NoticeKeys } from "../../generated/graphql";
 import { InputAddedEvent } from "@cartesi/rollups/dist/src/types/contracts/interfaces/IInput";
+import {
+    ACCOUNT_INDEX,
+    DAPP_ADDRESS,
+    DAPP_NAME,
+    HARDHAT_DEFAULT_MNEMONIC,
+    HARDHAT_DEFAULT_RPC_URL
+} from "./configs/constants";
 
-interface Args extends ConnectArgs, RollupsArgs {
+interface SendInputParams {
     input: string;
+}
+
+export interface SendInputViewModel {
+    epochNumber: InputMaybe<string> | undefined;
+    inputIndex: InputMaybe<string> | undefined;
 }
 
 export const findNoticeKeys = (receipt: ContractReceipt): NoticeKeys => {
@@ -25,37 +37,37 @@ export const findNoticeKeys = (receipt: ContractReceipt): NoticeKeys => {
     };
 };
 
-export const sendInput = async (args: Args) => {
-    const { rpc, input, mnemonic, accountIndex } = args;
+export const sendInput = async (params: SendInputParams): Promise<SendInputViewModel> => {
+    const { input } = params;
 
     // connect to provider
-    console.log(`connecting to ${rpc}`);
-    const { provider, signer } = connect(rpc, mnemonic, accountIndex);
-
-    const network = await provider.getNetwork();
-    console.log(`connected to chain ${network.chainId}`);
+    const { provider, signer } = connect(
+        HARDHAT_DEFAULT_RPC_URL,
+        HARDHAT_DEFAULT_MNEMONIC,
+        ACCOUNT_INDEX
+    );
+    await provider.getNetwork();
 
     // connect to rollups,
-    const { inputContract } = await rollups(signer || provider, args);
-
-    const signerAddress = await inputContract.signer.getAddress();
-    console.log(`using account "${signerAddress}"`);
-
-    // use message from command line option, or from user prompt
-    console.log(`sending "${input}"`);
+    const { inputContract } = await rollups(signer || provider, {
+        ...params,
+        dapp: DAPP_NAME,
+        address: DAPP_ADDRESS,
+    });
+    await inputContract.signer.getAddress();
 
     // convert string to input bytes
     const inputBytes = ethers.utils.toUtf8Bytes(input);
 
     // send transaction
     const tx = await inputContract.addInput(inputBytes);
-    console.log(`transaction: ${tx.hash}`);
-    console.log("waiting for confirmation...");
-    const receipt = await tx.wait(1);
+    const confirmationsCount = 1;
+    const receipt = await tx.wait(confirmationsCount);
 
     // find reference to notice from transaction receipt
     const noticeKeys = findNoticeKeys(receipt);
     return {
-        message: `input ${noticeKeys.input_index} added to epoch ${noticeKeys.epoch_index}`,
+        epochNumber: noticeKeys.epoch_index,
+        inputIndex: noticeKeys.input_index
     };
 };
