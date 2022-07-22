7d5afc21d7c8d2e9b77f937168b43ed3595bd492
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-07-22 13:56:56 -0300

    app test layout implemented

--- frontend-m2cgen/src/App.tsx
+++ frontend-m2cgen/src/App.tsx
@@ -1,33 +1,63 @@
-import { useEffect, useState } from "react";
 import styled from "styled-components";
-import { fetchNotices } from "./controller/use-service/use-service.functions";
+import { fetchNotices } from "./controller/notices.controller";
+import { sendInput, SendInputData } from "./controller/send.controller";
 import { useService } from "./controller/use-service/use-service.hook";
-import { getNotices, NoticeViewModel } from "./service/notices";
+import { NoticeViewModel } from "./service/notices.service";
+import { SendInputViewModel } from "./service/send.service";
 
 export const App = () => {
-    const [
-        { data, error, status },
-        dispatch
-    ] = useService<NoticeViewModel[]>();
+    const [ noticesState, noticesDispatch ] = useService<NoticeViewModel[]>();
+    const [ sendInputState, sendInputDispatch ] = useService<SendInputViewModel>();
 
-    useEffect(() => {
-        fetchNotices(dispatch)
-    }, [])
+    const handleResult = (notices: NoticeViewModel[]) => {
+        const notice = notices[0];
+        const message = notice.payload_parsed === '1' ? 'You survived!' : 'You sank';
+
+        return <h2>{message}</h2>;
+    }
+
+    const handleSendInput = (e: any) => {
+        e.preventDefault()
+        const form = e.target;
+        const elementsKeys = Object.keys(form);
+        let data: SendInputData = {
+            Age: null, Embarked: null, Sex: null
+        }
+        elementsKeys
+            .filter((key) => !!form[key]?.value)
+            .forEach((key) => {
+                const { id, value } = form[key]
+                const parsedId: keyof typeof data = id.replace('Input', '')
+                data[parsedId] = value;
+            });
+        sendInput(sendInputDispatch, data).then((result) =>
+            fetchNotices(noticesDispatch, {
+                epoch_index: result?.epochNumber,
+                input_index: result?.inputIndex,
+            }, true)
+        );
+    }
 
     return (
         <AppWrapper>
             <Heading>Hello world</Heading>
-            {!!data?.length ? (
-                <ul>
-                    {data.map((notice, idx) => (
-                        <li key={`${notice.notice_index}`}>
-                            {notice.payload_parsed}
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <h3>No notices available</h3>
-            )}
+            {!!noticesState.data?.length ? (
+                handleResult(noticesState.data)
+            ) : null}
+            <div>
+                <form
+                    onSubmit={handleSendInput}
+                    style={{ display: "flex", flexDirection: "column" }}
+                >
+                    <label htmlFor="ageInput">Age</label>
+                    <input id="AgeInput" type="number" />
+                    <label>Sex</label>
+                    <input id="SexInput" type="text" />
+                    <label>Embark</label>
+                    <input id="EmbarkedInput" type="text" />
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </AppWrapper>
     );
 };
