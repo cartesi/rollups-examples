f1d609dfa597fc3c98c83559a2dd2641271e7348
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-07-22 13:54:28 -0300

    notices and send controllers implemented

---
+++ frontend-m2cgen/src/controller/notices.controller.ts
@@ -0,0 +1,30 @@
+import { Dispatch } from "react";
+import { NoticeKeys } from "../../generated/graphql";
+import { getNotices, NoticeViewModel } from "../service/notices";
+import { genTimerPromise } from "../utils/timer-promise";
+import { REFETCH_TIME_DEFAULT } from "./config/constants";
+import { ServiceReducerActions } from "./use-service/use-service.hook";
+
+export const fetchNotices = async (
+    dispatch: Dispatch<ServiceReducerActions<NoticeViewModel[]>>,
+    params?: NoticeKeys,
+    refetchIfEmpty?: boolean
+) => {
+    dispatch({ type: "start_request" });
+    try {
+        const paramsFallback = params ?? { epoch_index: "0" };
+        let fetchedNotices: NoticeViewModel[] = await getNotices(paramsFallback);
+
+        if (refetchIfEmpty) while (!fetchedNotices.length) {
+            await genTimerPromise(REFETCH_TIME_DEFAULT);
+            const refetch = await getNotices(paramsFallback, true);
+            fetchedNotices = refetch;
+        }
+
+        dispatch({ type: "resolve_request", data: fetchedNotices });
+
+        return fetchedNotices;
+    } catch (err) {
+        dispatch({ type: "fail_request", error: err });
+    }
+};
---
+++ frontend-m2cgen/src/controller/send.controller.ts
@@ -0,0 +1,28 @@
+import { Dispatch } from "react";
+import { SendInputViewModel, sendInput as sendInputService } from "../service/send";
+import { ServiceReducerActions } from "./use-service/use-service.hook";
+
+export interface SendInputData {
+    Age: number | null;
+    Sex: string | null;
+    Embarked: string | null;
+}
+
+export const sendInput = async (
+    dispatch: Dispatch<ServiceReducerActions<SendInputViewModel>>,
+    data: SendInputData
+) => {
+    dispatch({ type: 'start_request' });
+    try {
+        const sendInputResult = await sendInputService({
+            input: JSON.stringify({ ...data, Age: Number(data.Age) })
+        });
+        dispatch({
+            type: 'resolve_request',
+            data: sendInputResult
+        });
+        return sendInputResult;
+    } catch (err) {
+        dispatch({ type: 'fail_request', error: err });
+    }
+};
