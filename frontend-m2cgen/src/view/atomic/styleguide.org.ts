b6ec5920f59377a2cd0cdde1c174379c56be2b9f
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-07-25 12:45:40 -0300

    atomic styleguid org created

---
+++ frontend-m2cgen/src/view/atomic/styleguide.org.ts
@@ -0,0 +1,22 @@
+const brandColor = {
+    main: "#1E1941",
+    lightMain: "#5E4E92",
+    mediumMain: "#332C63",
+};
+
+export const color = {
+    ...brandColor,
+    white: "#FFFFFF",
+    gray: "#9E9E9E",
+    mediumGray: "#7A7A7A",
+};
+
+export const typography = {
+    fontFamily: "'Rubik', sans-serif",
+    fontSize: "16px",
+    weight: {
+        light: "300",
+        regular: "400",
+        bold: "500"
+    }
+};
