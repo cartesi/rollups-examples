589f3db3814161a8d5fbe0c09d8faef53b1bf996
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-07-25 13:02:46 -0300

    global style mol implemented

--- frontend-m2cgen/src/main.tsx
+++ frontend-m2cgen/src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { GlobalStyle } from './view/atomic/global-style.mol';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
+        <GlobalStyle />
         <App />
     </React.StrictMode>
 );
