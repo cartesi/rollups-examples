dcdadccae558fd14ab6adc650264ebc99c633880
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-07-29 11:52:39 -0300

    remove console.log

--- frontend-m2cgen/src/view/layout/home/home-view.tsx
+++ frontend-m2cgen/src/view/layout/home/home-view.tsx
@@ -41,7 +41,6 @@ export const HomeView: FC = () => {
         resetServiceState(noticesDispatch);
         resetServiceState(sendInputDispatch);
     };
-    console.log({sendInputState, noticesState});
 
     return (
         <SharedLayout>
