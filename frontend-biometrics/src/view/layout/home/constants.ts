a86f20e379efe998ccb9ace88885fdf7ce1f301d
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-09-28 15:56:41 -0300

    biometrics gallery basic implemented

---
+++ frontend-biometrics/src/view/layout/home/constants.ts
@@ -0,0 +1,21 @@
+export const string = {
+    resultPreview: {
+        title: "Result preview",
+        idleFeedback: "Use the form to get started! 🧊🚢",
+        pendingFeedback: "Waiting for result...",
+    },
+    biometricsGallery: {
+        description: 'Lorem ipsum dolor sit amet'
+    },
+    sendInputFeedback: {
+        requestStarted:
+            "Sending infos, wait please. It may take a few minutes.",
+        web3OnboardError:
+            "You need to connect the wallet to proceed with this action!",
+        onError: "Sorry! An error occurred while send infos, try again later.",
+    },
+    fetchNoticesFeedback: {
+        onSucess: "Your result is ready!",
+        onError: "Sorry! An error occurred while get result, try again later.",
+    },
+};
