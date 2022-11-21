0d8929871e166c0daec18b37e19abc93d5f7c45e
Marcus Souza <souza.mvsl@gmail.com>
2022-11-21 16:12:05 -0300

    feat: Clear routines, UniqueID routine Treatment

--- biometrics/biometrics.py
+++ biometrics/biometrics.py
@@ -12,19 +12,48 @@
 
 from genericpath import exists
 from os import environ
-import os,sys
+import os, sys
 import model
 import json
 import traceback
 import logging
 import requests
 import base64
+import time
+
+
+startTime = time.time()
 
 logging.basicConfig(level="INFO")
 logger = logging.getLogger(__name__)
 
 rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
 logger.info(f"HTTP rollup_server url is {rollup_server}")
+
+
+def get_uptime():
+    """
+    Returns the number of seconds since the program started.
+    """
+    # do return startTime if you just want the process start time
+    return time.time() - startTime
+
+
+def clear_disk():
+    current_dir = os.getcwd()
+    list = os.listdir(current_dir)
+
+    # Delete histograms, png files and temporary files
+    for item in list:
+        if (
+            item.endswith("_hist.txt")
+            or item.endswith("_string64.txt")
+            or item.endswith(".png")
+        ):
+            os.remove(os.path.join(current_dir, item))
+    global startTime
+    startTime = time.time()
+
 
 def verify_payload(data):
     try:
@@ -34,19 +63,22 @@ def verify_payload(data):
     except:
         logger.info("Not a valid Payload for Image processing")
 
-def str642img(str,name):
+
+def str642img(str, name):
     try:
         imgdata = base64.b64decode(str)
-        with open(name, 'wb') as f:
+        with open(name, "wb") as f:
             f.write(imgdata)
     except:
         print("Error while decoding")
+
 
 def hex2str(hex):
     """
     Decodes a hex string into a regular string
     """
     return bytes.fromhex(hex[2:]).decode("utf-8")
+
 
 def str2hex(str):
     """
@@ -61,41 +93,49 @@ def input_treatment(cnt, eps=1e-7):
     """
     parts = []
     for i in range(len(cnt)):
-        cnt[i] = cnt[i].replace(';\n','')
-        cnt[i] = cnt[i].replace('\n','')
-        cnt[i] = cnt[i].replace('[','')
-        cnt[i] = cnt[i].replace(']','')
-        cnt[i] = cnt[i].replace(' ','')
-        cnt[i] = cnt[i].replace(","," ")
+        cnt[i] = cnt[i].replace(";\n", "")
+        cnt[i] = cnt[i].replace("\n", "")
+        cnt[i] = cnt[i].replace("[", "")
+        cnt[i] = cnt[i].replace("]", "")
+        cnt[i] = cnt[i].replace(" ", "")
+        cnt[i] = cnt[i].replace(",", " ")
         parts = cnt[i].split()
-    
-    #normalize histogram
+
+    # normalize histogram
     for i in range(len(parts)):
         parts[i] = float(parts[i])
     soma = sum(parts)
-    for i in range(len(parts)): 
-        parts[i] /=  soma + eps
+    for i in range(len(parts)):
+        parts[i] /= soma + eps
     return parts
 
 
-def classify(input): 
+def classify(input):
     """
     Predicts a given input's classification using the model generated with m2cgen
     """
     # computes the score from the input
     score = model.score(input)
-    if (score > 0):
-            finger_class = "Live"
+    if score > 0:
+        finger_class = "Live"
     else:
-            finger_class = "fake"
+        finger_class = "fake"
 
-    # returns the class 
+    # returns the class
     return finger_class
 
 
 def handle_advance(data):
     status = "accept"
-    
+    logger.info(f"Uptime : {get_uptime()}")
+
+    msg_sender = data["metadata"]["msg_sender"]
+    logger.info(f"Message Sender  : {msg_sender}")
+
+    # clears disc if the time is higher than 24 hours
+    if get_uptime() > 3600:
+        clear_disk()
+
     try:
         # retrieves input as string
         input_json = verify_payload(data)
@@ -103,60 +143,92 @@ def handle_advance(data):
             status = "reject"
             return status
 
-        #check if the image is divided in chunks or it is only one chunk
-        #Start check inputs. When we receive a chunk with final tag we turn loaded for true and call opencv. Otherwise we just add the content in the temporary file.
         loaded = False
-        text_fn = input_json["imageId"]+'.txt'
+
+        # check if the image is divided in chunks or it is only one chunk
+        # Start check inputs. When we receive a chunk with final tag we turn loaded for true and call opencv. Otherwise we just add the content in the temporary file.
+        text_fn = (
+            msg_sender
+            + "_"
+            + (os.path.splitext(input_json["imageId"])[0])
+            + "_string64.txt"
+        )
+        #check if file already exists. If true, check the current chunk. If the chunk is initial and the file exists, delete the existing file in disk.
+        if os.path.exists(text_fn):
+            if input_json["chunk"] == "initial":
+                os.remove(text_fn)
 
         with open(text_fn, "a") as text_file:
             text_file.write(input_json["content"])
-        if(input_json["chunk"]=="final"):
+        if input_json["chunk"] == "final":
             loaded = True
             print("You can now load opencv")
-
-        #if loaded = true, convert string64 to img and then calls opencv. At the end, turn loaded = false.
-        if(loaded):
+        # if loaded = true, convert string64 to img and then calls opencv. At the end, turn loaded = false.
+        if loaded:
             with open(text_fn) as f:
                 lines = f.read().rstrip()
-        #Convert the string to image
-            str642img(lines, input_json["imageId"])
-        #call opencv feature extractor binary to get the histogram
-            img_name = input_json["imageId"]
-            print(("The image name is: "+img_name))
-            os.environ['LD_LIBRARY_PATH'] = '/mnt/dapp/opencv/lib'
-            cmd = './fexrvv -i '+img_name
+            # Convert the string to image
+            img_name = msg_sender + "_" + input_json["imageId"]
+            str642img(lines, img_name)
+            # call opencv feature extractor binary to get the histogram
+            os.environ["LD_LIBRARY_PATH"] = "/mnt/dapp/opencv/lib"
+            cmd = "./fexrvv -i " + img_name
             so = os.popen(cmd).read()
             print(so)
             loaded = False
-        #reads the generated histogram
-            hist_file = open("hist.txt", "r")
+            # reads the generated histogram
+            img_hist = (
+                msg_sender
+                + "_"
+                + (os.path.splitext(input_json["imageId"])[0])
+                + "_hist.txt"
+            )
+            os.rename("hist.txt", img_hist)
+            hist_file = open(img_hist, "r")
             hist = hist_file.readlines()
-        #Treats the histogram generated by c++
+            # Treats the histogram generated by c++
             hist = input_treatment(hist)
-        # computes predicted classification for input        
+            # computes predicted classification for input
             predicted = classify(hist)
-            logger.info(f"Data={img_name}, Predicted: {predicted}")
+            logger.info(f"Data ={img_name}, Predicted: {predicted}")
+            # delete all files created to clean space
+            os.remove(img_name)
+            os.remove(img_hist)
+            os.remove(text_fn)
 
-        # emits output notice with predicted class name
+            # emits output notice with predicted class name
             output = str2hex(str(predicted))
             logger.info(f"Adding notice with payload: {predicted}")
-            response = requests.post(rollup_server + "/notice", json={"payload": output})
-            logger.info(f"Received notice status {response.status_code} body {response.content}")
+            response = requests.post(
+                rollup_server + "/notice", json={"payload": output}
+            )
+            logger.info(
+                f"Received notice status {response.status_code} body {response.content}"
+            )
         else:
-        # emits output notice with "Image not loaded"
+            # emits output notice with "Image not loaded"
             output = str2hex("Image not loaded")
             logger.info(f"Adding notice with payload: {output}")
-            response = requests.post(rollup_server + "/notice", json={"payload": output})
-            logger.info(f"Received notice status {response.status_code} body {response.content}")
+            response = requests.post(
+                rollup_server + "/notice", json={"payload": output}
+            )
+            logger.info(
+                f"Received notice status {response.status_code} body {response.content}"
+            )
 
     except Exception as e:
         status = "reject"
         msg = f"Error processing data {data}\n{traceback.format_exc()}"
         logger.error(msg)
-        response = requests.post(rollup_server + "/report", json={"payload": str2hex(msg)})
-        logger.info(f"Received report status {response.status_code} body {response.content}")
+        response = requests.post(
+            rollup_server + "/report", json={"payload": str2hex(msg)}
+        )
+        logger.info(
+            f"Received report status {response.status_code} body {response.content}"
+        )
 
     return status
+
 
 def handle_inspect(data):
     logger.info(f"Received inspect request data {data}")
@@ -165,6 +237,7 @@ def handle_inspect(data):
     response = requests.post(rollup_server + "/report", json=report)
     logger.info(f"Received report status {response.status_code}")
     return "accept"
+
 
 handlers = {
     "advance_state": handle_advance,
