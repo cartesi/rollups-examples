78210f07b40e5df9ee98984bae10a0220aaf31a0
Francisco Moura <francisco.demoura@cartesi.io>
2023-01-11 17:15:40 -0300

    feat: Add ERC20 contract to common-contracts

---
+++ common-contracts/contracts/auction/SimpleERC20.sol
@@ -0,0 +1,25 @@
+// Copyright 2021 Cartesi Pte. Ltd.
+
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software distributed
+// under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
+/// @title A Simple ERC-20 Contract
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SimpleERC20 is ERC20 {
+    constructor(
+        address minter,
+        uint256 initialSupply
+    ) ERC20("SimpleERC20", "SIM20") {
+        _mint(minter, initialSupply);
+    }
+}
