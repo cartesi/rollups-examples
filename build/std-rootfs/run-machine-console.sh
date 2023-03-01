8d3960224114fe60e30230fae98d1274ae036f65
Milton Jonathan <milton.jonathan@cartesi.io>
2023-03-01 17:30:23 -0300

    feat: Update dependencies used in std-rootfs builds

--- build/std-rootfs/run-machine-console.sh
+++ build/std-rootfs/run-machine-console.sh
@@ -32,8 +32,8 @@ cartesi-machine \
     --ram-length=128Mi \
     --rollup \
     --flash-drive=label:dapp,filename:dapp.ext2 \
-    --flash-drive=label:root,filename:rootfs-v0.15.0.ext2 \
+    --flash-drive=label:root,filename:rootfs-v0.16.0.ext2 \
     --ram-image=linux-5.15.63-ctsi-1.bin \
-    --rom-image=rom-v0.13.0.bin \
+    --rom-image=rom-v0.15.0.bin \
     -i $CM_OPTS \
     -- "/bin/sh"
