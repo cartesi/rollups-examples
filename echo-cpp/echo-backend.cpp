f343d4e1a06a809c413d2e2c2ea2de0f4a9ab1f4
Francisco Moura <francisco.demoura@cartesi.io>
2023-04-25 14:12:25 -0300

    chore: correct code formating

--- echo-cpp/echo-backend.cpp
+++ echo-cpp/echo-backend.cpp
@@ -16,8 +16,7 @@
 #include "3rdparty/cpp-httplib/httplib.h"
 #include "3rdparty/picojson/picojson.h"
 
-std::string handle_advance(httplib::Client &cli, picojson::value data)
-{
+std::string handle_advance(httplib::Client &cli, picojson::value data) {
     std::cout << "Received advance request data " << data << std::endl;
     std::cout << "Adding notice" << std::endl;
     auto payload = data.get("payload").get<std::string>();
@@ -27,8 +26,7 @@ std::string handle_advance(httplib::Client &cli, picojson::value data)
     return "accept";
 }
 
-std::string handle_inspect(httplib::Client &cli, picojson::value data)
-{
+std::string handle_inspect(httplib::Client &cli, picojson::value data) {
     std::cout << "Received inspect request data " << data << std::endl;
     std::cout << "Adding report" << std::endl;
     auto payload = data.get("payload").get<std::string>();
@@ -38,8 +36,7 @@ std::string handle_inspect(httplib::Client &cli, picojson::value data)
     return "accept";
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
     std::map<std::string, decltype(&handle_advance)> handlers = {
         {std::string("advance_state"), &handle_advance},
         {std::string("inspect_state"), &handle_inspect},
@@ -47,18 +44,14 @@ int main(int argc, char **argv)
     httplib::Client cli(getenv("ROLLUP_HTTP_SERVER_URL"));
     cli.set_read_timeout(20, 0);
     std::string status("accept");
-    while (true)
-    {
+    while (true) {
         std::cout << "Sending finish" << std::endl;
         auto finish = std::string("{\"status\":\"") + status + std::string("\"}");
         auto r = cli.Post("/finish", finish, "application/json");
         std::cout << "Received finish status " << r.value().status << std::endl;
-        if (r.value().status == 202)
-        {
+        if (r.value().status == 202) {
             std::cout << "No pending rollup request, trying again" << std::endl;
-        }
-        else
-        {
+        } else {
             picojson::value rollup_request;
             picojson::parse(rollup_request, r.value().body);
 
