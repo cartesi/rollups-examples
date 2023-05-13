d6e8f564f7b3427a43f8ba3b346b843be0359c82
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-05-13 23:07:16 +0000

    codroipo x2

---
+++ nix/flake/flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {};
+  outputs = {nixpkgs, ...}: let
+    pkgs = nixpkgs.legacyPackages.riscv64-linux;
+  in {
+    packages.riscv64-linux.default = builtins.toFile
+      "hello-world"
+      "Hello world!";
+  };
+
+}
